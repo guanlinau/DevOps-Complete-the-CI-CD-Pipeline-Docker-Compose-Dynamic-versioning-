faab8d9335fe0af212d8cb9426d3d8872e9cf0e5
JasonCaoAU <jason2019au@gmail.com>
2023-02-27 16:49:14 +1100

    initial commit

--- script.groovy
+++ script.groovy
@@ -1,7 +1,31 @@
 def buildJar() {
     echo "building the application..."
-    sh 'mvn package'
+    sh 'mvn clean package'
 } 
+def incrementVersion() {
+   sh 'mvn build-helper:parse-version versions:set \
+   -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} \
+   versions:commit'
+   def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
+   def version = matcher[0][1]
+   env.IMAGE_NAME = "$version-$BUILD_NUMBER-java-multi-pipeline-amd64"
+   echo "${IMAGE_NAME}"
+}
+
+def commitVersionUpdate(){
+    withCredentials([usernamePassword(credentialsId: 'Jenkins-github-pat', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'git config --global user.email "jason.guanlin.cao@gmail.com"'
+        sh 'git config --global user.name "jason"'
+        sh 'git status'
+        sh 'git branch'
+
+        sh "git remote set-url origin https://${USER}:${PASS}@github.com/CGL-DevOps/DevOps-Dynamically-Increment-Application-version-in-Jenkins-Pipeline.git"
+        sh 'git add .'
+        echo "${USER} ${PASS}"
+        sh 'git commit -m "ci:version bump ${BUILD_NUMBER}"'
+        sh 'git push origin HEAD:master'
+    }
+}
 
 def buildImage() {
     echo "building the docker image..."
